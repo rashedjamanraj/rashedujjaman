438d3fa98ba1ec9bfaaafa1551bcfc2c0a33ae9d
rashedjamanraj <rashed.jaman.raj@gmail.com>
2026-08-03 00:39:36 +0600

    rashedujjaman

---
+++ components/StartPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StartPage = () => {
+  return (
+    <div className=' max-w-7xl mx-auto px-20 py-16'>
+      <h2 className=' text-3xl font-bold mb-4'>Hi, I am Rashedujjaman</h2>
+      <h3 className=' text-xl font-bold mb-4'>Full-Stack JavaScript Developer | Building modern, responsive & high-converting web apps & websites for global businesses</h3>
+    </div>
+  )
+}
+
+export default StartPage
